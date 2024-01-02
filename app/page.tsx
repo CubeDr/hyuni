6713c0b7886a39f2059b3b0baa11324543b62eb9
Hyuni Kim <spaceship00@naver.com>
2024-01-02 15:09:41 +0000

    Apply styling to intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import { Courgette } from 'next/font/google'
+
+const courgette = Courgette({ subsets: ['latin'], weight: '400' });
 
 export default function Page() {
   return (
     <div className='main-column'>
-      <div className={styles.intro}>
-        <div className={styles.introMain}>
-          <div className={styles.hi}>Greetings!</div>
-          <div>I'm Hyuni, a software engineer believing a good code makes a good product.</div>
+      <div className={styles.Intro}>
+        <div className={styles.IntroMain}>
+          <div className={styles.Hi}>Greetings!</div>
+          <div>I'm <b>Hyuni</b>,<br />a software engineer believing</div>
+          <div className={styles.IntroQuote + ' ' + courgette.className}>a Good Code<br />makes<br />a Good Product</div>
         </div>
-        <img src='https://placehold.co/150x180' />
+        <img
+          className={styles.IntroImage}
+          src='https://firebasestorage.googleapis.com/v0/b/hyuni-website.appspot.com/o/hyuni.jpg?alt=media&token=49a8927a-7606-457d-8107-da0fce03b3ff' />
       </div>
     </div>
   )
-}
+};
