60afc6e995566328a48bf84bf17eb4aa8e63f353
Hyuni Kim <spaceship00@naver.com>
2024-01-06 03:22:04 +0000

    Add contents to cube module

--- app/component/module/cube/cubeModule.tsx
+++ app/component/module/cube/cubeModule.tsx
@@ -1,9 +1,27 @@
 import Module, { DefaultModuleProps } from '../module';
+import styles from './cube.module.css';
+import cubeImage from '../../../../public/images/cube-compressed.jpg';
+import Image from 'next/image';
 
 export default function CubeModule({ className }: DefaultModuleProps) {
     return (
-        <Module title='Cube'>
-            Cube Module
+        <Module title='Rubiks Cube' className={className}>
+            <div className={styles.Body}>
+                <Image
+                    className={styles.Image}
+                    src={cubeImage}
+                    alt='Cube'
+                    width={90}
+                    height={120} />
+                <div className={styles.Content}>
+                    3x3 Avg. 25s<br />
+                    3x3 Best 14.257s<br /><br />
+                    Also...
+                    <span className={styles.Also}>
+                        Pyraminx, Megaminx, 2x2x2, 4x4x4, 5x5x5, Square-1, Mastermorphix
+                    </span>
+                </div>
+            </div>
         </Module>
     );
 }
