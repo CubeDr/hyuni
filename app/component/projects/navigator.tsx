48a6ef700b8a966460dc9a739bb5b3b74da3914b
CubeDr <jbhyunikim@gmail.com>
2024-03-10 22:10:04 +0900

    Show mandelbrot in project list

--- app/component/projects/navigator.tsx
+++ app/component/projects/navigator.tsx
@@ -1,39 +1,48 @@
 'use client'
 
-import { MouseEvent, useState } from 'react';
+import {MouseEvent, useState} from 'react';
 import styles from './projects.module.css';
 
 interface NavigatorProps {
-    projects: string[],
+  projects: string[];
+  selectedProject: string;
+  onClick: (project: string) => void;
 }
 
-export default function Navigator({ projects }: NavigatorProps) {
-    const [indicatorTop, setIndicatorTop] = useState(0);
+export default function Navigator({projects, selectedProject, onClick}: NavigatorProps) {
+  const [indicatorTop, setIndicatorTop] = useState(projects.indexOf(selectedProject));
 
-    function onProjectNameHover(e: MouseEvent) {
-        const target = e.target as HTMLElement;
-        setIndicatorTop(target.offsetTop);
-    }
+  function onProjectNameHover(e: MouseEvent) {
+    const target = e.target as HTMLElement;
+    setIndicatorTop(target.offsetTop);
+  }
 
-    return (
-        <div className={styles.Navigator}>
-            <div className={styles.Indicator}
-                style={{
-                    top: indicatorTop,
-                }}></div>
-            <ul className={styles.ProjectList}>
-                {projects.map(project => {
-                    return (
-                        <li
-                            id={'project-' + project}
-                            key={'project-' + project}
-                            className={styles.ProjectName}
-                            onMouseEnter={onProjectNameHover}>
-                            {project}
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
-    );
+  function onProjectNameLeave() {
+    const target = document.getElementById('project-' + selectedProject) as HTMLElement;
+    setIndicatorTop(target.offsetTop);
+  }
+
+  return (
+    <div className={styles.Navigator}>
+      <div className={styles.Indicator}
+           style={{
+             top: indicatorTop,
+           }}></div>
+      <ul className={styles.ProjectList}>
+        {projects.map(project => {
+          return (
+            <li
+              id={'project-' + project}
+              key={'project-' + project}
+              className={styles.ProjectName}
+              onMouseEnter={onProjectNameHover}
+              onMouseLeave={onProjectNameLeave}
+              onClick={() => onClick(project)}>
+              {project}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
--- app/component/projects/projects.tsx
+++ app/component/projects/projects.tsx
@@ -3,32 +3,36 @@
 import styles from './projects.module.css';
 import Navigator from './navigator';
 import JBTimer from './jbtimer/jbtimer';
+import {useCallback, useState} from 'react';
 
 const PROJECTS = [
-    'JBTimer',
-    // 'Hyminton',
-    // 'AutoMatch',
-    // 'DotPlace',
-    // 'AutoTrade',
-    // 'DeveloperDungeon',
+  'JBTimer',
+  'Mandelbrot',
+  // 'Hyminton',
+  // 'AutoMatch',
+  // 'DotPlace',
+  // 'AutoTrade',
+  // 'DeveloperDungeon',
 ];
 
 export default function Projects() {
-    return (
-        <section className={styles.Projects}>
-            <div className={styles.ProjectsControl}>
-                <h1>Projects</h1>
-                <Navigator projects={PROJECTS} />
-            </div>
-            <div className={styles.ProjectDetailContainer}>
-                <div className={styles.ProjectDetail}>
-                    <div className={styles.ProjectDetailFrame}>
-                        <div className={styles.ProjectDetailFrameUpperLeftCorder}></div>
-                        <div className={styles.ProjectDetailFrameLowerRightCorder}></div>
-                        <JBTimer />
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  const [selectedProject, setSelectedProject] = useState(PROJECTS[0]);
+
+  return (
+    <section className={styles.Projects}>
+      <div className={styles.ProjectsControl}>
+        <h1>Projects</h1>
+        <Navigator projects={PROJECTS} selectedProject={selectedProject} onClick={setSelectedProject}/>
+      </div>
+      <div className={styles.ProjectDetailContainer}>
+        <div className={styles.ProjectDetail}>
+          <div className={styles.ProjectDetailFrame}>
+            <div className={styles.ProjectDetailFrameUpperLeftCorder}></div>
+            <div className={styles.ProjectDetailFrameLowerRightCorder}></div>
+            <JBTimer/>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
