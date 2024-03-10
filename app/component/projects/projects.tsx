6bba9496b8d497bd7f4d5d9b33ea7ec8f0872329
CubeDr <jbhyunikim@gmail.com>
2024-03-10 22:48:54 +0900

    Add Mandelbrot project

--- app/component/projects/projects.tsx
+++ app/component/projects/projects.tsx
@@ -3,6 +3,7 @@
 import styles from './projects.module.css';
 import Navigator from './navigator';
 import JBTimer from './jbtimer/jbtimer';
+import Mandelbrot from './mandelbrot/mandelbrot';
 import {useEffect, useRef, useState} from 'react';
 
 const PROJECTS = [
@@ -17,6 +18,7 @@ const PROJECTS = [
 
 export default function Projects() {
   const [selectedProject, setSelectedProject] = useState(PROJECTS[0]);
+  const [projectElement, setProjectElement] = useState<JSX.Element>(<JBTimer/>);
   const container = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -24,12 +26,22 @@ export default function Projects() {
       if (container?.current?.classList?.contains(styles.CloseAnimation)) {
         container.current.classList.remove(styles.CloseAnimation);
       }
+
+      let element = <>To be updated</>;
+      switch (selectedProject) {
+        case 'JBTimer':
+          element = <JBTimer/>;
+          break;
+        case 'Mandelbrot':
+          element = <Mandelbrot/>;
+      }
+      setProjectElement(element);
     };
 
     container?.current?.addEventListener('animationend', onAnimationEnd);
 
     return () => container?.current?.removeEventListener('animationend', onAnimationEnd);
-  }, [container]);
+  }, [container, selectedProject]);
 
   useEffect(() => {
     container?.current?.classList.add(styles.CloseAnimation);
@@ -46,7 +58,7 @@ export default function Projects() {
           <div className={styles.ProjectDetailFrame}>
             <div className={styles.ProjectDetailFrameUpperLeftCorder}></div>
             <div className={styles.ProjectDetailFrameLowerRightCorder}></div>
-            <JBTimer/>
+            {projectElement}
           </div>
         </div>
       </div>
