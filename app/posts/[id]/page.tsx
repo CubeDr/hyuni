b687788447c2d087a96e8efd8d29ddef38cd0b01
CubeDr <spaceship00@naver.com>
2024-10-10 23:16:05 +0900

    Show post content

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { getPost } from '@/firebase/posts';
 import styles from './page.module.css';
 import { timestampToString } from '@/utils/time';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 interface Props {
   params: {
@@ -19,6 +21,13 @@ export default async function PostPage({ params: { id } }: Props) {
         <span>{timestampToString(timestamp)}</span>
       </div>
       <hr className={styles.Divider} />
+      <div className={styles.Content}>
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]}
+        >
+          {blocks[0].content}
+        </ReactMarkdown>
+      </div>
     </div>
   );
 }
