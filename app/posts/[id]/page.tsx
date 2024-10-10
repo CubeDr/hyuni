52c6cfc2ffd7ffe7818f78375d6cffed44b0d4ea
CubeDr <spaceship00@naver.com>
2024-10-10 23:30:18 +0900

    Generate post page metadata

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,13 +1,28 @@
 import { getPost } from '@/firebase/posts';
-import styles from './page.module.css';
 import { timestampToString } from '@/utils/time';
+import { Metadata } from 'next';
 import ReactMarkdown from 'react-markdown';
+import { remark } from 'remark';
 import remarkGfm from 'remark-gfm';
+import strip from 'strip-markdown';
+import styles from './page.module.css';
 
 interface Props {
   params: {
     id: string;
   }
+}
+
+export async function generateMetadata({ params: { id } }: Props): Promise<Metadata> {
+  const { title, category, blocks } = await getPost(id);
+  const description = await remark().use(strip).process(blocks[0].content);
+  return {
+    title,
+    description: description.toString().replaceAll('\n\n', '\n').trim(),
+    authors: { 'name': 'Hyuni Kim', url: 'https://hyuni.dev' },
+    category,
+    creator: 'Hyuni Kim',
+  };
 }
 
 export default async function PostPage({ params: { id } }: Props) {
