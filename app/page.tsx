5ea3e38eb2e375e9568f3d1c39a0ee316f8b58c9
CubeDr <spaceship00@naver.com>
2024-10-10 18:16:33 +0900

    Do not show projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Articles from "@/app/component/articles/Articles";
 import Intro from './component/intro/intro';
 import Modules from './component/module/modules';
-import Projects from './component/projects/projects';
 
 export default function Page() {
   return (
@@ -9,7 +8,6 @@ export default function Page() {
       <Intro />
       <Modules />
       <Articles />
-      <Projects />
     </div>
   )
 };
