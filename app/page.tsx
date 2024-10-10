c76f36b8925daa41f138ba8497766d0f8d6d5293
CubeDr <spaceship00@naver.com>
2024-10-10 15:10:54 +0900

    Remove badminton, cube modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,26 @@
+import Articles from "@/app/component/articles/Articles";
 import Intro from './component/intro/intro';
 import AuthoredModule from './component/module/authored/authoredModule';
-import BadmintonModule from './component/module/badminton/badmintonModule';
-import CubeModule from './component/module/cube/cubeModule';
 import ExperienceModule from './component/module/experience/experienceModule';
 import InterviewModule from './component/module/interview/interviewModule';
 import SkillsModule from './component/module/skills/skillsModule';
 import Projects from './component/projects/projects';
 import styles from './page.module.css';
-import Articles from "@/app/component/articles/Articles";
 
 export default function Page() {
   return (
     <div className='main-column'>
-      <Intro/>
+      <Intro />
       <div className={styles.ThreeColumnGrid}>
-        <ExperienceModule className={styles.ExperienceModule}/>
+        <ExperienceModule className={styles.ExperienceModule} />
         <div>
-          <SkillsModule/>
-          <AuthoredModule/>
+          <SkillsModule />
+          <AuthoredModule />
         </div>
-        <InterviewModule/>
+        <InterviewModule />
       </div>
-      <div className={styles.TwoColumnGrid}>
-        <BadmintonModule/>
-        <CubeModule/>
-      </div>
-      <Articles/>
-      <Projects/>
+      <Articles />
+      <Projects />
     </div>
   )
 };
