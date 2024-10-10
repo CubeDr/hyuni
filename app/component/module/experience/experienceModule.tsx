3a3ec5d8d7fe8e3d74a9441fbaecb315d7cd6924
CubeDr <spaceship00@naver.com>
2024-10-10 18:13:29 +0900

    Make modules responsive

--- app/component/module/experience/experienceModule.tsx
+++ app/component/module/experience/experienceModule.tsx
@@ -1,13 +1,24 @@
 import Module, { DefaultModuleProps } from '../module';
 import ExperienceItem from './experienceItem';
+import styles from './ExperienceModule.module.css';
 
 export default function ExperienceModule({ className }: DefaultModuleProps) {
     return (
         <Module title='Experience' className={className}>
-            <ExperienceItem title='Google' role='Software Engineer' type='FTE' period='2020 Jun~NOW' isOngoing={true} />
-            <ExperienceItem title='Line Studio' role='Backend Engineer' type='Intern' period='2020 Jan~Feb' isOngoing={false} />
-            <ExperienceItem title='Line Studio' role='Frontend Engineer' type='Intern' period='2019 Jan~Feb' isOngoing={false} />
-            <ExperienceItem title='Samsung Electronics' role='VR Engineer' type='Intern' period='2016 Jan~Feb' isOngoing={false} />
+            <div className={styles.Body}>
+                <div className={styles.Item}>
+                    <ExperienceItem title='Google' role='Software Engineer' type='FTE' period='2020 Jun~NOW' isOngoing={true} />
+                </div>
+                <div className={styles.Item}>
+                    <ExperienceItem title='Line Studio' role='Backend Engineer' type='Intern' period='2020 Jan~Feb' isOngoing={false} />
+                </div>
+                <div className={styles.Item}>
+                    <ExperienceItem title='Line Studio' role='Frontend Engineer' type='Intern' period='2019 Jan~Feb' isOngoing={false} />
+                </div>
+                <div className={styles.Item}>
+                    <ExperienceItem title='Samsung Electronics' role='VR Engineer' type='Intern' period='2016 Jan~Feb' isOngoing={false} />
+                </div>
+            </div>
         </Module>
     );
 }
