871b0691097c680dd6dab769a860c8b0740499d8
CubeDr <spaceship00@naver.com>
2024-10-10 21:11:29 +0900

    Upload post to firestore

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,13 +1,32 @@
 'use client';
 
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Editor from './Editor';
 import styles from './page.module.css';
 import Preview from './Preview';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '@/firebase/firebaseClient';
 
 export default function WritePage() {
   const [title, setTitle] = useState('');
   const [value, setValue] = useState('');
+
+  const submit = useCallback(async () => {
+    try {
+      await addDoc(collection(db, 'posts'), {
+        title,
+        blocks: [
+          {
+            type: 'markdown',
+            content: value,
+          }
+        ],
+        timestamp: new Date().getTime(),
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }, [value]);
 
   return (
     <div className={styles.WritePage}>
@@ -20,7 +39,7 @@ export default function WritePage() {
       </div>
       <div className={styles.ControlRow}>
         <input placeholder='카테고리' />
-        <button className={styles.SubmitButton}>게시</button>
+        <button className={styles.SubmitButton} onClick={submit}>게시</button>
       </div>
     </div>
   );
