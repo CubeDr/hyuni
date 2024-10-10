5747dd7f3451dd2b1d6c24941647a05985179811
CubeDr <spaceship00@naver.com>
2024-10-10 20:01:00 +0900

    Add markdown preview

---
+++ app/write/Preview.tsx
@@ -0,0 +1,19 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import styles from './Preview.module.css';
+
+interface Props {
+  content: string;
+}
+
+export default function Preview({ content }: Props) {
+  return (
+    <div className={styles.Preview}>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
+  );
+}
--- app/write/page.tsx
+++ app/write/page.tsx
@@ -2,13 +2,16 @@
 
 import { useState } from 'react';
 import Editor from './Editor';
+import styles from './page.module.css';
+import Preview from './Preview';
 
 export default function WritePage() {
   const [value, setValue] = useState('');
 
   return (
-    <>
+    <div className={styles.WritePage}>
       <Editor value={value} onChange={setValue} />
-    </>
+      <Preview content={value} />
+    </div>
   );
 }
