d188054dfd3a21e3ab3c2a9991e331980e0407eb
CubeDr <spaceship00@naver.com>
2024-11-30 12:42:33 +0900

    Do not show placeholder series title in post item

--- app/posts/PostItem.tsx
+++ app/posts/PostItem.tsx
@@ -9,7 +9,7 @@ export default function PostItem({ post }: Props) {
   return (
     <a href={`/posts/${post.id}`} className={styles.PostItem}>
       <img className={styles.Thumbnail} src='https://picsum.photos/200/100' />
-      <div className={styles.Category}>{post.category} &gt; 게임 만들기</div>
+      <div className={styles.Category}>{post.category}</div>
       <h2 className={styles.Title}>{post.title}</h2>
       <div className={styles.Content}>{post.blocks[0].content}</div>
       <div className={styles.Timestamp}>{timestampToString(post.timestamp)}</div>
