62b15f954cb76910ef59e2e0d1f8bab1bbd71f56
CubeDr <spaceship00@naver.com>
2024-12-04 20:52:44 +0900

    Add metadata to the post page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from 'next';
 import AppBar from '../component/appbar/AppBar';
 import PostGrid from './PostGrid';
+
+export const metadata: Metadata = {
+  title: 'Posts :: 현이의 개발 이야기',
+  description: '"좋은 코드가 좋은 제품을 만든다"를 믿는 개발자입니다.',
+  authors: [{ name: '김현이', url: 'https://www.hyuni.dev' }],
+}
 
 export default async function Posts() {
   return (
