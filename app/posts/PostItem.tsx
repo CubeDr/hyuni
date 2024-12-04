555e3e8dc82b1f0cb50cc78ddf1880e4bd687017
CubeDr <spaceship00@naver.com>
2024-12-04 22:00:01 +0900

    Show posts in list on small screens

--- app/posts/PostItem.tsx
+++ app/posts/PostItem.tsx
@@ -9,10 +9,12 @@ export default function PostItem({ post }: Props) {
   return (
     <a href={`/posts/${post.id}`} className={styles.PostItem}>
       <img className={styles.Thumbnail} src={post.thumbnailImageSrc} />
-      <div className={styles.Category}>{post.category}</div>
-      <h2 className={styles.Title}>{post.title}</h2>
-      <div className={styles.Content}>{post.blocks[0].content}</div>
-      <div className={styles.Timestamp}>{timestampToString(post.timestamp)}</div>
+      <div>
+        <div className={styles.Category}>{post.category}</div>
+        <h2 className={styles.Title}>{post.title}</h2>
+        <div className={styles.Content}>{post.blocks[0].content}</div>
+        <div className={styles.Timestamp}>{timestampToString(post.timestamp)}</div>
+      </div>
     </a>
   );
 }
