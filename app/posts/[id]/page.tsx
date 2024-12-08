926b2b3075f05f138cc3ff5e25de1042a14816a1
CubeDr <spaceship00@naver.com>
2024-12-08 19:39:02 +0900

    Disable syntax highlight for now
    This is breaking the pages with code.

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -44,7 +44,6 @@ export default async function PostPage({ params: { id } }: Props) {
     <>
       <AppBar />
       <PostViewer post={post} />
-      <SyntaxHighlighter />
     </>
   );
 }
--- next.config.js
+++ next.config.js
@@ -9,7 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  transpilePackages: ['highlight.js'],
 };
 
 module.exports = nextConfig;
