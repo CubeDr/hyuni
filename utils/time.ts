46550affa907941e4d8dacbb7b38344f4a763fe6
CubeDr <spaceship00@naver.com>
2024-12-12 11:38:23 +0900

    Format date time in KST

--- utils/time.ts
+++ utils/time.ts
@@ -1,10 +1,5 @@
-export function timestampToString(timestamp: number) {
-  const fullDate = new Date(timestamp);
-  const year = fullDate.getFullYear();
-  const month = (fullDate.getMonth() + 1).toString().padStart(2, '0');
-  const date = fullDate.getDate().toString().padStart(2, '0');
-  const hour = fullDate.getHours().toString().padStart(2, '0');
-  const minute = fullDate.getMinutes().toString().padStart(2, '0');
+import { format } from 'date-fns-tz';
 
-  return `${year}. ${month}. ${date}. ${hour}:${minute}`;
+export function timestampToString(timestamp: number) {
+  return format(timestamp, 'yyyy. MM. dd. HH:mm', { timeZone: 'Asia/Seoul' });
 }
