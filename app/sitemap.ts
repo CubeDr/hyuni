7013d5b2d31b0a96c52e267bb7911db80165bd1e
CubeDr <spaceship00@naver.com>
2024-12-15 12:20:42 +0900

    Make sitemap.xml dynamic

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,6 +5,8 @@ import { MetadataRoute } from 'next';
 
 const BASE_URL = 'https://hyuni.dev';
 
+export const dynamic = 'force-dynamic';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await getPosts();
 
