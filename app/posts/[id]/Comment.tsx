9621cf6b6168593ec2941847a09cb9876634b12d
CubeDr <spaceship00@naver.com>
2024-12-23 17:49:18 +0900

    Show comment button

--- app/posts/[id]/Comment.tsx
+++ app/posts/[id]/Comment.tsx
@@ -2,12 +2,14 @@
 
 import { AuthContext } from '@/firebase/AuthContext';
 import Image from 'next/image';
-import { FormEvent, useCallback, useContext, useRef, useState } from 'react';
+import { FormEvent, useCallback, useContext, useState } from 'react';
 import styles from './Comment.module.css';
 
 export default function Comment() {
   const { user } = useContext(AuthContext);
   const [comment, setComment] = useState('');
+
+  const isActive = comment.trim() !== '';
 
   const onChange = useCallback((e: FormEvent<HTMLDivElement>) => {
     setComment(e.currentTarget.innerText);
@@ -22,20 +24,23 @@ export default function Comment() {
   }
 
   return (
-    <div className={styles.Comment}>
-      <Image
-        className={styles.Photo}
-        src={user!.photoURL!}
-        width={50}
-        height={50}
-        alt={user!.displayName ?? user.email ?? '유저'} />
-      <div className={styles.Box}>
-        <span className={styles.DisplayName}>{user?.displayName}</span>
-        <div
-          className={styles.Input + (comment.trim() === '' ? ' ' + styles.Empty : '')}
-          contentEditable
-          onInput={onChange} />
+    <>
+      <div className={styles.Comment}>
+        <Image
+          className={styles.Photo}
+          src={user!.photoURL!}
+          width={44}
+          height={44}
+          alt={user!.displayName ?? user.email ?? '유저'} />
+        <div className={styles.Box}>
+          <span className={styles.DisplayName}>{user?.displayName}</span>
+          <div
+            className={styles.Input + (isActive ? '' : ' ' + styles.Empty)}
+            contentEditable
+            onInput={onChange} />
+        </div>
       </div>
-    </div>
+      <div className={styles.Button + (isActive ? '' : ' ' + styles.Inactive)}>등록</div>
+    </>
   );
 }
