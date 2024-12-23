56943f454deed4be81b8081dde88d290f2b03e04
CubeDr <spaceship00@naver.com>
2024-12-23 21:25:44 +0900

    Show login in footer instead of separate page

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,9 +1,15 @@
 import styles from './Footer.module.css';
+import Login from './Login';
 
 export default function Footer() {
   return (
     <footer className={styles.Footer}>
-      Copyright &copy; {new Date().getFullYear()} Hyuni Kim. All rights reserved.
+      <span className={styles.Login}>
+        <Login />
+      </span>
+      <div className={styles.Copyright}>
+        Copyright &copy; {new Date().getFullYear()} Hyuni Kim. All rights reserved
+      </div>
     </footer>
   );
 }
--- app/Login.tsx
+++ app/Login.tsx
@@ -2,15 +2,13 @@
 
 import { AuthContext } from '@/firebase/AuthContext';
 import { auth } from '@/firebase/firebaseClient';
-import { Button } from '@mui/material';
 import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import { useContext } from 'react';
-import AppBar from '../component/appbar/AppBar';
+import { useCallback, useContext } from 'react';
 
-export default function LoginPage() {
+export default function Login() {
   const { user } = useContext(AuthContext);
 
-  const signInWithGoogle = async () => {
+  const login = useCallback(async () => {
     const provider = new GoogleAuthProvider();
     try {
       const result = await signInWithPopup(auth, provider);
@@ -33,29 +31,18 @@ export default function LoginPage() {
     } catch (error) {
       console.error('Sign-in error:', error);
     }
-  };
+  }, []);
 
-  const signOutUser = async () => {
+  const logout = useCallback(async () => {
     try {
       await signOut(auth);
       window.alert('Signed out successfully!');
     } catch (error) {
       console.error('Sign-out error:', error);
     }
-  };
+  }, []);
 
-  return (
-    <>
-      <AppBar />
-      {user ? (
-        <Button variant="contained" onClick={signOutUser}>
-          로그아웃
-        </Button>
-      ) : (
-        <Button variant="contained" onClick={signInWithGoogle}>
-          구글 로그인
-        </Button>
-      )}
-    </>
-  );
+  return <>
+    <span onClick={user ? logout : login}>{user ? '로그아웃' : '로그인'}</span>
+  </>;
 }
