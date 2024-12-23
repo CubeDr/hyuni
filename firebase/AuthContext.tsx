be1beeb87f875c0fd21fa4903ef6e5482dc56e52
CubeDr <spaceship00@naver.com>
2024-12-23 22:37:22 +0900

    Only sign up when agreed to privacy

--- firebase/AuthContext.tsx
+++ firebase/AuthContext.tsx
@@ -2,6 +2,7 @@
 
 import { auth } from '@/firebase/firebaseClient';
 import { Member } from '@/types/member';
+import { Checkbox } from '@mui/material';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -9,7 +10,7 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { onAuthStateChanged, signOut, User } from 'firebase/auth';
-import { createContext, useCallback, useEffect, useState } from 'react';
+import { ChangeEvent, createContext, useCallback, useEffect, useState } from 'react';
 import { getMember, setMember as setMemberFirebase } from './members';
 
 interface AuthContextType {
@@ -26,6 +27,7 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
   const [isLoaded, setIsLoaded] = useState(false);
 
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isPricayAgreed, setIsPrivacyAgreed] = useState(false);
 
   const updateMember = useCallback((user: User | null) => {
     if (user == null) {
@@ -57,7 +59,7 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
   }, []);
 
   const signup = useCallback(() => {
-    if (user == null) return;
+    if (user == null || !isPricayAgreed) return;
 
     setMemberFirebase({
       id: user.uid,
@@ -68,6 +70,10 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
       setIsDialogOpen(false);
     });
   }, [user, updateMember]);
+
+  const onPrivacyCheckboxChange = useCallback((e: ChangeEvent) => {
+    setIsPrivacyAgreed((e.target as HTMLInputElement).checked);
+  }, []);
 
   return (
     <>
@@ -86,14 +92,19 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
             가입되어 있지 않은 계정입니다. 회원 가입 하시겠습니까?
+            <br />
+            <Checkbox onChange={onPrivacyCheckboxChange} />
+            <a target="_blank" href="/privacy" style={{
+              color: 'var(--color-primary)'
+            }}>개인 정보 처리 방침</a>을 확인하였습니다.
           </DialogContentText>
         </DialogContent>
         <DialogActions>
           <Button onClick={logout} color='inherit'>
             취소
           </Button>
-          <Button onClick={signup}>
-            확인
+          <Button onClick={signup} disabled={!isPricayAgreed}>
+            회원 가입
           </Button>
         </DialogActions>
       </Dialog>
