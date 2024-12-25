eeb9f35d77dddc6967e1b9f029d5c8cb70e45c24
CubeDr <spaceship00@naver.com>
2024-12-25 11:20:21 +0900

    Only allow java

--- app/posts/[id]/SyntaxHighlighter.tsx
+++ app/posts/[id]/SyntaxHighlighter.tsx
@@ -3,33 +3,24 @@
 import './androidstudio.css';
 
 import hljs from 'highlight.js';
-import css from "highlight.js/lib/languages/css";
 import java from "highlight.js/lib/languages/java";
-import javascript from "highlight.js/lib/languages/javascript";
-import json from "highlight.js/lib/languages/json";
-import kotlin from "highlight.js/lib/languages/kotlin";
-import python from "highlight.js/lib/languages/python";
-import sql from "highlight.js/lib/languages/sql";
-import typescript from "highlight.js/lib/languages/typescript";
-import xml from "highlight.js/lib/languages/xml";
-import yaml from "highlight.js/lib/languages/yaml";
 import { useEffect } from 'react';
 
 export default function SyntaxHighlighter() {
   useEffect(() => {
-    hljs.registerLanguage("javascript", javascript);
-    hljs.registerLanguage("typescript", typescript);
-    hljs.registerLanguage("js", javascript);
-    hljs.registerLanguage("ts", typescript);
+    // hljs.registerLanguage("javascript", javascript);
+    // hljs.registerLanguage("typescript", typescript);
+    // hljs.registerLanguage("js", javascript);
+    // hljs.registerLanguage("ts", typescript);
     hljs.registerLanguage("java", java);
-    hljs.registerLanguage("kotlin", kotlin);
-    hljs.registerLanguage("json", json);
-    hljs.registerLanguage("html", xml);
-    hljs.registerLanguage("xml", xml);
-    hljs.registerLanguage("python", python);
-    hljs.registerLanguage("css", css);
-    hljs.registerLanguage("sql", sql);
-    hljs.registerLanguage("yaml", yaml);
+    // hljs.registerLanguage("kotlin", kotlin);
+    // hljs.registerLanguage("json", json);
+    // hljs.registerLanguage("html", xml);
+    // hljs.registerLanguage("xml", xml);
+    // hljs.registerLanguage("python", python);
+    // hljs.registerLanguage("css", css);
+    // hljs.registerLanguage("sql", sql);
+    // hljs.registerLanguage("yaml", yaml);
 
     hljs.highlightAll();
   }, []);
