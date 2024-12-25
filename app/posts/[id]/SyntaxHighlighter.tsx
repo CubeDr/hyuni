4087a6f7291e2b90f79aed1676d6fe2a1a51694a
CubeDr <spaceship00@naver.com>
2024-12-25 11:06:25 +0900

    Add to transpile and only register specific languages

--- app/posts/[id]/SyntaxHighlighter.tsx
+++ app/posts/[id]/SyntaxHighlighter.tsx
@@ -3,7 +3,26 @@
 import './androidstudio.css';
 
 import hljs from 'highlight.js';
+import css from "highlight.js/lib/languages/css";
+import javascript from "highlight.js/lib/languages/javascript";
+import json from "highlight.js/lib/languages/json";
+import python from "highlight.js/lib/languages/python";
+import sql from "highlight.js/lib/languages/sql";
+import typescript from "highlight.js/lib/languages/typescript";
+import xml from "highlight.js/lib/languages/xml";
+import yaml from "highlight.js/lib/languages/yaml";
 import { useEffect } from 'react';
+
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("typescript", typescript);
+hljs.registerLanguage("js", javascript);
+hljs.registerLanguage("json", json);
+hljs.registerLanguage("html", xml);
+hljs.registerLanguage("xml", xml);
+hljs.registerLanguage("python", python);
+hljs.registerLanguage("css", css);
+hljs.registerLanguage("sql", sql);
+hljs.registerLanguage("yaml", yaml);
 
 export default function SyntaxHighlighter() {
   useEffect(() => {
