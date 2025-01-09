880269be7cf5a21dc4fb39391b4d98dd99ba9ab9
CubeDr <spaceship00@naver.com>
2025-01-09 21:58:37 +0900

    Add naver site verification

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Articles from "@/app/component/articles/Articles";
+import { Metadata } from 'next';
 import Intro from './component/intro/intro';
 import Modules from './component/module/modules';
 import PostGrid from './posts/PostGrid';
@@ -6,6 +7,12 @@ import Section from './Section';
 import ShortLinks from './ShortLinks';
 
 export const dynamic = 'force-dynamic';
+
+export const metadata: Metadata = {
+  other: {
+    'naver-site-verification': '58dca89ab063aa7d6f8a110cbb6c27218e7ac05a',
+  },
+};
 
 export default function Page() {
   return (
